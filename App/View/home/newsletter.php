67a6917bacb8f2f7c0a0aa6947c625f0e91bfd1e
Dilaksa Shanmugathas <Dilaksa.Shanmugathas@pebe.ch>
2020-06-04 10:57:54 +0200

    delete funktion
    Signed-off-by: Dilaksa Shanmugathas <Dilaksa.Shanmugathas@pebe.ch>

--- App/View/home/newsletter.php
+++ App/View/home/newsletter.php
@@ -1,11 +1,12 @@
 <form action="/Newsletter/public/home/newsletter" method="POST">
 	<b><i>Newsletter</i></b><br><br/>
-	
+
 	Vorname:  	<input name="Vorname" type="text" /><br><br/>
 	Nachname: 	<input name="Nachname" type="text" /><br><br/>	
 	Strasse: 	<input name="Strasse" type="text" /> <br><br/>
 	Ort: 		<input name="Ort" type="text" /><br /><br/>
     E-mail: 	<input name="Email" type="text" /><br /> <br/>
 	<button type="submit" value="Submit">Submit</button>
+
     
 </form>
